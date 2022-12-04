967efaefa821f831e99149d342bcd6161252835f
Daniel <danielmordelfer@gmail.com>
2022-12-04 22:56:13 +0000

    nova classe recipe e alterações no Item and Inventory

---
+++ Recipe.py
@@ -0,0 +1,19 @@
+
+class recipe:
+
+    def __init__(self, name, materials, quantities, description):
+        self.name = name
+        self.materials = materials
+        self.quantities = quantities
+        self.description = description
+    
+
+    def display(self):
+        print("Recipe")
+        print("   Name: " + self.name)
+        print("   Description " + self.description)
+
+
+
+
+
